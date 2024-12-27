f4e81dd7b3d82b4b78f245dbfcfadd09d0a9e2aa
hojang <26dev@naver.com>
2024-12-27 22:23:26 +0900

    스프링 앱에서 데이터 소스 사용

---
+++ src/main/java/org/example/spring_start_here/ex11/controller/PurchaseController.java
@@ -0,0 +1,29 @@
+package org.example.spring_start_here.ex11.controller;
+
+import org.example.spring_start_here.ex11.model.Purchase;
+import org.example.spring_start_here.ex11.repository.PurchaseRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseController(PurchaseRepository purchaseRepository){
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase){
+        purchaseRepository.storePurchase(purchase);
+    }
+
+    @GetMapping
+    public List<Purchase> findPurchases(){
+        return purchaseRepository.findAllPurchases();
+    }
+
+}
---
+++ src/main/java/org/example/spring_start_here/ex11/model/Purchase.java
@@ -0,0 +1,14 @@
+package org.example.spring_start_here.ex11.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class Purchase {
+    private int id;
+    private String product;
+    private BigDecimal price;
+}
